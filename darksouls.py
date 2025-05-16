de60c26038a5bd48afeff081c65e1434e3354d1d
Federico <47462792+wtfzambo@users.noreply.github.com>
2025-05-16 22:01:37 +0200

    feat: add test running logic

--- darksouls.py
+++ darksouls.py
@@ -1,15 +1,75 @@
-# 1. Find if test exists at all
-#    1.1. If not, you died
-# 2. Run tests
-#    2.1. If all pass, you died
-#    2.x. If any fail, you died
-# 3. Return output
-
 import argparse
+import enum
+import os
+import subprocess
+
+os.system("color")
+
+RED_DARK = "\033[31m"
+END = "\033[0m"
+
+YOU_DIED = """
+⢰⣶⣶⣤⣤⡄⠀⠀⠀⠀⢰⣶⣶⣶⣶⠀⢀⣠⣶⣿⡆⣿⣷⣦⣄⠀⠀⠀⠀⢶⣶⣶⣶⣶⡆⠀⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⢶⣶⣶⣶⢠⣶⣶⣶⣶⣦⣄⠀⠀⠀⠀⣶⣶⣶⣶⣶⠰⣿⣷⣶⣶⣶⣶⣶⣶⡶⢶⣶⣶⡆⣶⣶⣶⣶⣶⣤⣄⠀⠀⠀⠀
+⠀⠈⢿⣿⣿⣇⠀⠀⠀⠀⠀⣽⣿⡿⠉⣴⣿⣿⠟⠋⠀⠀⠈⠛⣿⣷⣆⠀⠀⠀⢻⣿⣿⠁⠀⠀⠀⠀⠀⠈⣿⣿⡏⠀⠀⠀⠀⠀⠀⢹⣿⣿⠈⠉⠉⠙⠻⣿⣿⣷⣆⠀⠀⠈⣿⣿⣿⠁⠀⠀⣿⣿⣿⠉⠙⢻⣿⡅⠀⣿⣿⡇⠙⠛⠛⠻⢿⣿⣿⣷⣄⠀⠀
+⠀⠀⠀⠻⣿⣿⡄⠀⠀⠀⣼⣿⠟⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠀⠈⢿⣿⣆⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠹⣿⣿⣦⠀⠀⢻⣿⡇⠀⠀⠀⣿⣿⣿⠀⠀⠈⠉⠁⠀⣿⣿⡇⠀⠀⠀⠀⠀⠈⢻⣿⣿⣆⠀
+⠀⠀⠀⠀⠹⣿⣿⡀⠀⣸⣿⠏⠀⢰⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⡄⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇⠀⢸⣿⡇⠀⠀⠀⣿⣿⡟⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡄
+⠀⠀⠀⠀⠀⢻⣿⣷⣰⣿⠏⠀⠀⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⢸⣿⡇⠀⠀⠀⣿⣿⣷⣤⣤⣾⣿⠇⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇
+⠀⠀⠀⠀⠀⠈⢻⣿⣿⡿⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⢿⣿⡇⠀⠀⠀⣿⣿⣿⠛⠻⣿⣿⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇
+⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⢹⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡟⠀⢸⣿⡿⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⣸⣿⡇⠀⠀⠀⣿⣿⡇⠀⠀⠘⠛⠀⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠃
+⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠈⢿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠇⠀⠸⣿⣿⠀⠀⠀⠀⠀⠀⢀⣿⣿⠃⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⣿⣿⡇⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⠀
+⠀⠀⠀⠀⠀⠀⢈⣿⣿⡇⠀⠀⠀⠀⠈⠻⣿⣧⡀⠀⠀⠀⠀⠀⢀⣾⣿⠏⠀⠀⠀⢻⣿⣧⡀⠀⠀⠀⢀⣼⣿⡟⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡆⠀⠀⠀⠀⢀⣴⣿⣿⠇⠀⠀⣿⣿⣧⠀⠀⠀⣿⣿⣧⠀⠀⠀⢰⣦⠀⢿⣿⣿⠀⠀⠀⠀⠀⢀⣼⣿⣿⠃⠀
+⠀⠀⠀⠀⠀⠀⣼⣿⣿⣷⣄⠀⠀⠀⠀⠀⠙⢿⣿⣶⣦⣤⣴⣶⣿⡿⠋⠀⠀⠀⠀⠀⠻⣿⣿⣶⣶⣶⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣷⣦⣤⣴⣶⣿⣿⠟⠁⠀⠀⣴⣿⣿⣿⣄⡀⣴⣿⣿⣿⣷⣤⣶⣿⡇⣠⣿⣿⣿⣷⣶⣶⣶⣶⣿⡿⠟⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠋⠉⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠛⠋⠉⠉⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠁⠉⠉⠉⠙⠛⠛⠛⠉⠁⠉⠉⠛⠛⠛⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀
+"""
 
 
-def run_tests():
-    pass
+class ExitCode(enum.IntEnum):
+    SUCCESS = 0
+    FAILURE = 1
+    NOT_REPO = 0
+
+
+def find_repo_root(path: str = ".") -> str | None:
+    current_path = os.path.abspath(path)
+    while True:
+        if os.path.isdir(os.path.join(current_path, ".git")):
+            return current_path
+        parent_path = os.path.dirname(current_path)
+        if parent_path == current_path:
+            return None
+        current_path = parent_path
+
+
+def you_died():
+    print(RED_DARK + YOU_DIED + END)
+
+
+def run_tests(test_command: str):
+    repo_root = find_repo_root(os.getcwd())
+    if not repo_root:
+        print("Not in a git repository.")
+        return ExitCode.NOT_REPO
+
+    print(f"Dark Souls: Running test command '{test_command}' in {repo_root}")
+
+    try:
+        command_parts = test_command.split()
+        result = subprocess.run(
+            command_parts,
+            cwd=repo_root,  # Run the command from the repo root
+            check=False,  # Don't raise CalledProcessError on non-zero exit
+        )
+
+        if result.returncode == ExitCode.SUCCESS:
+            print("Dark Souls: Tests passed! Your code is safe... this time.")
+            return ExitCode.SUCCESS
+        else:
+            print("Dark Souls: Tests failed. You died.")
+            return ExitCode.FAILURE
+
+    except Exception as e:
+        print(f"Dark Souls: An error occurred: {e}")
+        return ExitCode.FAILURE
 
 
 def main():
@@ -18,14 +78,19 @@ def main():
     parser.add_argument(
         "--test-command",
         required=True,
+        type=str,
         help="The command to execute to run tests (e.g. 'npm test', 'pytest', etc.)",
     )
 
     args = parser.parse_args()
+    exit_code = run_tests(args.test_command)  # pyright: ignore[reportAny]
 
-    print("Hello from darksouls!")
-    print(f"{args=}")
-    return 0
+    if exit_code is ExitCode.FAILURE:
+        you_died()
+    else:
+        print("Dark Souls: You have proven yourself worthy.")
+
+    return exit_code
 
 
 if __name__ == "__main__":
